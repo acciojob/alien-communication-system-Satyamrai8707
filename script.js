234e2cb544b4dd9fb9fdd86c191377cdcc6664da
Satyam Rai <114270778+Satyamrai8707@users.noreply.github.com>
2026-01-20 19:12:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+function log(message) {
+  output.innerHTML += message + "<br>";
+  console.log(message);
+}
+
+// Synchronous task
+log("🛸 Alien signal received (Synchronous Task)");
+
+// Macrotask
+setTimeout(() => {
+  log("📡 Processing large alien transmission (Macrotask)");
+}, 0);
+
+// Microtask
+Promise.resolve().then(() => {
+  log("⚡ Decoding alien micro-signal (Microtask)");
+});
+
+// Another synchronous task
+log("👨‍🚀 Human response prepared (Synchronous Task)");
